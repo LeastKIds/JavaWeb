107dd9a465d185992c9bb03e360c7a986693d54e
LeastKIds <leastkids@gmail.com>
2020-06-04 00:35:23 +0900

    200603

---
+++ 200603/Student.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+public class Student
+{
+  int studentId;
+  String studentName;
+  String adress;
+
+  public void showStudentInfo()
+  {
+    System.out.println(studentName + "," + adress);
+  }
+}
---
+++ 200603/studentTest.java
@@ -0,0 +1,13 @@
+package chapter5;
+
+public class studentTest
+{
+  public static void main(String[] args)
+  {
+    Student studentLee=new Student();
+    studentLee.studentName="Lee";
+    studentLee.adress="soeul";
+
+    studentLee.showStudentInfo();
+  }
+}
